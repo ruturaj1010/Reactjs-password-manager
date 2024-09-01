d0c6e7f7b697e9f1686b98cc2e9dd4e877fc033b
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-09-01 23:14:12 +0530

    build navbar and set bg img

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
+import React from "react"
+import Navbar from './components/Navbar'
+import Manager from "./components/Manager"
 
 function App () {
 
   return (
     <>
-      <div className="container">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
+      <Navbar/>
+      <Manager/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex justify-around items-center px-3 h-14 bg-blue-950 text-gray-300 w-screen'>
+      <div className='logo font-bold cursor-none'>PWManager</div>
+      <ul>
+        <li className='flex gap-10 items-center w-60'>
+          <a href="#" className='text-lg hover:font-semibold'>Home</a>
+          <a href="#" className='text-lg hover:font-semibold'>About</a>
+          <a href="#" className='text-lg hover:font-semibold'>Contact</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
